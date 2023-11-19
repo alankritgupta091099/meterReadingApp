3d2e1091e7072b00992cd52181af0cbee203e381
alankritgupta091099 <alankritgupta091099@gmail.com>
2023-11-19 17:32:50 -0500

    updated user account screen

--- screens/UserAccount.js
+++ screens/UserAccount.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, ScrollView } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 import { UserContext } from '../context/userContext';
@@ -17,7 +17,20 @@ function UserAccount(){
     
     return (
         <SafeAreaView style={{flex: 1, justifyContent: 'center'}}>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <ScrollView
+            showsVerticalScrollIndicator={false}
+            style={{paddingHorizontal: 25}}>
+            <Text
+                style={{
+                    fontFamily: 'Roboto-Medium',
+                    fontSize: 28,
+                    fontWeight: '500',
+                    color: '#333',
+                    marginVertical: 30,
+                }}>
+                User Account
+            </Text>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>                
                 <FontAwesome name="user-circle-o" size={60} color={'grey'} />
                 <Text style={{marginTop: 10, marginBottom:40}}>User ID: {user.UserID}</Text>
                 <CustomButton 
@@ -25,6 +38,7 @@ function UserAccount(){
                     onPress={logout} 
                 />
             </View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
